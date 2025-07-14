c2d0a70296a7205162679ead4a1e88e251552a9c
jeancarlo022 <jeancarlor224@gmail.com>
2025-07-14 16:23:30 -0500

    hpla

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Saludo from './Saludo';
+import Contador from './contador';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> className="App"
+      <h1>¡Hola React!</h1>
+      <p>Este es mi primer componente.</p>
+
+      <Saludo nombre= "Santiago"/>
+      <Saludo nombre= "Laura"/>
+      {/*Aquí va el contador*/}
+        <Contador />
     </div>
   );
 }
---
+++ src/Saludo.js
@@ -0,0 +1,5 @@
+function Saludo(props) {
+  return <h2>¡Bienvenido al mundo de React, {props.nombre}!</h2>;
+}
+export default Saludo;
+
